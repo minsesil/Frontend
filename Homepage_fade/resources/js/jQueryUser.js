e85dac9142594e9879b13bb6bfcf830e43c9f209
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-18 17:55:13 +0900

    Create jQueryUser.js

---
+++ Homepage_fade/resources/js/jQueryUser.js
@@ -0,0 +1,33 @@
+$(()=>{
+    // 네비 각각 내려오기
+    /*
+    $('.nav>li').mouseover(function() {
+      $(this).children('.submenu').stop().slideDown();
+    });
+    $('.nav>li').mouseout(function() {
+      $(this).children('.submenu').stop().slideUp();
+    });
+    */
+  
+    // 네비 전체 내려오기
+    $('nav').mouseover(function() {
+      $('.submenu').stop().slideDown();
+    });
+    $('nav').mouseout(function() {
+      $('.submenu').stop().slideUp();
+    });
+  
+    
+    // img slide
+    let x = $('#imgslide>img');
+    let count = 3;
+    setInterval(function() {
+      count--;
+      if(count==0) {
+        x.fadeIn();
+        count = 3;  //중요 다시 넣어줌
+      } else {
+        x.eq(count).fadeOut();
+      }
+    }, 2000);
+  })
